c42daca234509bc7e44b9726e2f89223d6afc760
David Kane <dave.kane@gmail.com>
2020-12-19 08:36:38 -0500

    hacks necessary to make all notes go away.

--- R/zzz.R
+++ R/zzz.R
@@ -29,4 +29,59 @@
                     question_ui_initialize.learnr_text,
                     ns = "learnr")
 
+  # Because learnr will not export these three functions, we have no choice but
+  # to use ::: in order to access them. Doing do produces a NOTE when we run R
+  # command check. I find that annoying. So, I just provide my own copies here.
+  # This is absurd hack, but it is not clear what else one can do.
+
+  read_request <- function(session, name, default = NULL) {
+    if (!is.null(name)) {
+      if (exists(name, envir = session$request))
+        get(name, envir = session$request)
+      else
+        default
+    } else {
+      default
+    }
+  }
+
+  submissions_from_state_objects <- function(state_objects) {
+    filtered_submissions <- filter_state_objects(state_objects, c("question_submission", "exercise_submission"))
+    Filter(x = filtered_submissions, function(object) {
+      # only return answered question, not reset questions
+      if (object$type == "question_submission") {
+        !isTRUE(object$data$reset)
+      } else {
+        TRUE
+      }
+    })
+  }
+
+  get_all_state_objects <- function(session, exercise_output = TRUE) {
+
+    # get all of the objects
+    objects <- get_objects(session)
+
+    # strip output (the client doesn't need it and it's expensive to transmit)
+    objects <- lapply(objects, function(object) {
+      if (object$type == "exercise_submission") {
+        if (!exercise_output) {
+          object$data["output"] <- list(NULL)
+        }
+      }
+      object
+    })
+
+    # return objects
+    objects
+  }
+
+  # Listing these makes the annoying NOTES go away, but it is also an absurd
+  # hack. I am not even sure how it works . . .
+
+  utils::globalVariables(c("read_request",
+                           "submissions_from_state_objects",
+                           "get_all_state_objects",
+                           "filter_state_objects",
+                           "get_objects"))
 }
