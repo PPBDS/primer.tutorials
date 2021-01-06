67c2a198dcb5f0ebb91e960de0aed769a387129c
David Kane <dave.kane@gmail.com>
2021-01-05 22:21:15 -0600

    ready for version 0.1
    new checks to confirm Information and Submission sections

--- tests/testthat/test-tutorial.R
+++ tests/testthat/test-tutorial.R
@@ -1,22 +1,28 @@
 # Any code in a tutorial code chunk, including in a hint, which produces an
 # error when run on its own will cause an error if you use R CMD check. Most
 # common example is when you have ... to indicate to the student that they
-# should replace the ... with something. Best solution is to set eval=FALSE in
+# should replace the ... with something. Best solution is to set eval = FALSE in
 # all hint chunks like this. But the same warning applies for all chunks.
 
 # Testing is hard. There are (at least) two different environments in which we
 # run tests. First, and most important, is when we run R CMD check. This is the
-# most rigorous check. Most importantly, it seems to require that code written
-# in all code chunks work correctly. So, you can't just have things like
+# most rigorous check. It requires that code written in all code chunks
+# (including hints) work correctly. So, you can't just have things like
 # library(...) around, even though that is an extremely convenient way to
 # indicate to the student that they need to use the library function with some
-# argument. 
+# argument.
 
 # The second environment is when you press "Test Package". I think that all this
 # does is to source tests/testthat.R. (And this environment is (mostly) the same
 # as when you just execute tests interactively. At least, the two give the same
 # answer.) But, in this second case, the code in hint (and all other?) chunks is
-# not evaluated.
+# not evaluated. The same problem arises if you just use "Run Document," as
+# students will do (in effect) when the press Start Tutorial.
+
+# I think that, in order to simulate the test that is run with R CMD check, you
+# need, from the R Console, to:
+
+# rmarkdown::render("inst/tutorials/03-functions-A/tutorial.Rmd")
 
 # Side note: Because we use the system.package hack, we are only ever testing
 # the tutorials which are already installed. If you make a change in a file, you
@@ -36,11 +42,10 @@
 # not sure if render() is the same thing.
 
 # Fourth, need to update this for testtthat 3.0. Some of the new features should
-# allow us to make our tests more thorough. Also, I think that context() is
-# deprecated. Also, do we need to load primary.data here? Why?
+# allow us to make our tests more thorough.
 
-context("Tutorials")
 library(primer.tutorials)
+library(tidyverse)
 library(fs)
 
 base <- system.file(package = "primer.tutorials")
@@ -49,6 +54,38 @@ files <- fs::dir_ls(base, recurse = TRUE, regexp = "tutorial.Rmd") %>%
   fs::path_abs()
 
 stopifnot(length(files) > 3)
+
+# Maybe put this set of tests in another file. Goal is to make sure that each
+# tutorial includes the required Information and Submission sections.
+# Unfortunately, I could not figure out how to check that a multiple-line chunk
+# of code is present in a file.
+
+# So, instead, we will read in the Information and Submission section templates
+# with readLines, which puts each line as a separate item in a text vector. Then
+# we can confirm that all those lines are in each tutorial. Note that this does
+# not confirm that they are all together and in the correct order! But it is
+# good enough.
+
+information_lines <- readLines(
+  paste0(system.file("www/", package = "primer.tutorials"),
+         "information_check.txt"))
+
+for(i in files){
+  if(! all(information_lines %in% readLines(i))){
+    stop("Information lines missing from file ", i, "\n")
+  }
+}
+
+submission_lines <- readLines(
+  paste0(system.file("www/", package = "primer.tutorials"),
+         "submission_check.txt"))
+
+for(i in files){
+  if(! all(submission_lines %in% readLines(i))){
+    stop("Submission lines missing from file ", i, "\n")
+  }
+}
+
 
 # You can choose to run just a few of the portfolios through by selecting a
 # subset of files, like `files <- files[c(2)]` or `files <- files[c(1, 3:9)]`.
@@ -62,8 +99,7 @@ stopifnot(length(files) > 3)
 # library(primer.tutorials) in the code chunk for a tutorial, it will fail for a
 # user because it won't find submission_ui. But such a flawed tutorial.Rmd will
 # pass this test because we load library(primary.tutorials) before we start the
-# test above. Maybe this is just an edge case we can ignore. Why do we even need
-# to do library(primer.tutorials) above? 
+# test above. Maybe this is just an edge case we can ignore.
 
 for(i in files){
   test_that(paste("rendering", i), {
@@ -71,3 +107,4 @@ for(i in files){
                   "tutorial.html")
   })
 }
+
