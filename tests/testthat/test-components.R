9e998801a07d28de3932b458154637e1fac8135a
David Kane <dave.kane@gmail.com>
2021-01-06 22:29:03 -0600

    re-organize test code

---
+++ tests/testthat/test-components.R
@@ -0,0 +1,43 @@
+context("Confirm Information and Submission sections are correct")
+
+library(primer.tutorials)
+library(tidyverse)
+library(fs)
+
+base <- system.file(package = "primer.tutorials")
+
+files <- fs::dir_ls(base, recurse = TRUE, regexp = "tutorial.Rmd") %>%
+  fs::path_abs()
+
+stopifnot(length(files) > 3)
+
+# Goal is to make sure that each tutorial includes the required Information and
+# Submission sections. Unfortunately, I could not figure out how to check that a
+# multiple-line chunk of code is present in a file.
+
+# So, instead, we will read in the Information and Submission section templates
+# with readLines(), which puts each line as a separate item in a text vector.
+# Then we can confirm that all those lines are in each tutorial. Note that this
+# does not confirm that they are all together and in the correct order! But it
+# is good enough.
+
+information_lines <- readLines(
+  paste0(system.file("www/", package = "primer.tutorials"),
+         "information_check.txt"))
+
+for(i in files){
+  if(! all(information_lines %in% readLines(i))){
+    stop("Information lines missing from file ", i, "\n")
+  }
+}
+
+submission_lines <- readLines(
+  paste0(system.file("www/", package = "primer.tutorials"),
+         "submission_check.txt"))
+
+for(i in files){
+  if(! all(submission_lines %in% readLines(i))){
+    stop("Submission lines missing from file ", i, "\n")
+  }
+}
+
---
+++ tests/testthat/test-render.R
@@ -0,0 +1,42 @@
+# Once you know where the tutorial.Rmd file is, you have to "test" it somehow.
+# As you can see, our definition of "test" is to run render() and hope there is
+# no error. There is no check to see if "tutorial.html" looks OK, just that that
+# string is returned.
+
+# Might we do more here? For example, what we really want to confirm is that,
+# when a student presses the "Run Document" button, things will work. I am not
+# sure if render() is the same thing.
+
+context("Confirm that tutorials can be rendered")
+library(primer.tutorials)
+library(tidyverse)
+library(fs)
+
+base <- system.file(package = "primer.tutorials")
+
+files <- fs::dir_ls(base, recurse = TRUE, regexp = "tutorial.Rmd") %>%
+  fs::path_abs()
+
+stopifnot(length(files) > 3)
+
+# You can choose to run just a few of the portfolios through by selecting a
+# subset of files, like `files <- files[c(2)]` or `files <- files[c(1, 3:9)]`.
+# But I am not sure if that really works. For example, it seems like I could get
+# just file 2 to work or just all the other files to work but 2. Yet, whenever I
+# did them all, I got errors (all from within file 2). Not sure what to make of
+# this, other than that perhaps I should explore the new version of testthat.
+
+# This test is not complete because it does not simulate the scenario in which a
+# user runs a tutorial directly. For example, if you forget to include
+# library(primer.tutorials) in the code chunk for a tutorial, it will fail for a
+# user because it won't find submission_ui. But such a flawed tutorial.Rmd will
+# pass this test because we load library(primary.tutorials) before we start the
+# test above. Maybe this is just an edge case we can ignore.
+
+for(i in files){
+  test_that(paste("rendering", i), {
+    expect_output(rmarkdown::render(i, output_file = "tutorial.html"),
+                  "tutorial.html")
+  })
+}
+
