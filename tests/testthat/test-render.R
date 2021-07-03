f00c4a382cb82b9c969235009ccaa2a7850d7a97
David Kane <dave.kane@gmail.com>
2021-07-02 21:13:23 -0400

    rationalize the testing approach

--- tests/testthat/test-render.R
+++ tests/testthat/test-render.R
@@ -1,33 +1,24 @@
 library(primer.tutorials)
 library(tidyverse)
-library(learnr)
 
 # Our definition of "test" for a tutorial file is to run render() and hope there
 # is no error. There is no check to see if "tutorial.html" looks OK, just that
 # that string is returned.
 
 # Might we do more here? For example, what we really want to confirm is that,
-# when a student presses the "Run Document" button, things will work. I am not
+# when a student presses the "Start Tutorial" button, things will work. I am not
 # sure if render() is the same thing. But, the good news is that this test seems
 # much more robust than that. In other words, it catches things that do not
-# cause (immediate) failures with Run Document.
+# cause (immediate) failures with Start Tutorial.
 
 # See the discussions.txt file for some thoughts on what this is doing and how
 # we might improve it.
 
 
-tut_list <- available_tutorials("primer.tutorials")
-
-stopifnot(nrow(tut_list) > 15)
-
-for(i in tut_list$name){
+for(i in tutorial_paths()){
   cat(paste("Testing tutorial:", i, "\n"))
-  tut_path <- paste0(system.file("tutorials", package = "primer.tutorials"),
-                     "/",
-                     i,
-                     "/tutorial.Rmd")
-  test_that(paste("rendering", tut_path), {
-    expect_output(rmarkdown::render(tut_path, output_file = "tutorial.html"),
+  test_that(paste("rendering", i), {
+    expect_output(rmarkdown::render(i, output_file = "tutorial.html"),
                   "tutorial.html")
   })
 }
@@ -37,7 +28,7 @@ for(i in tut_list$name){
 # lot of tutorials and they take a while to run. There is not a lot to be done
 # about that. The second problem is that there is no way to customize it.
 # Sometimes, we would just like to test one tutorial, especially while we are
-# working on it. Other times, we want to test all the tutorials but one. 
+# working on it. Other times, we want to test all the tutorials but one.
 
 # This test is not complete because it does not simulate the scenario in which a
 # user runs a tutorial directly. For example, if you forget to include
