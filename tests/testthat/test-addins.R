e24b82aa3e30124d0db0857692400c1edfbd2c14
NuoWenLei <nuowen0612@gmail.com>
2021-07-28 19:50:06 +0800

    create install_only_binary_packages function add format_tutorial and new function to export

--- tests/testthat/test-addins.R
+++ tests/testthat/test-addins.R
@@ -6,7 +6,7 @@ format_tutorial_paths <- Sys.glob("addin_test_inputs/format*")
 
 for (i in seq_along(format_tutorial_paths)){
 
-  test_doc <- primer.tutorials:::format_tutorial(paste0("addin_test_inputs/format_input_", i, ".Rmd"))
+  test_doc <- primer.tutorials::format_tutorial(paste0("addin_test_inputs/format_input_", i, ".Rmd"))
 
   output_doc <- readr::read_file(paste0("addin_test_outputs/format_output_", i, ".Rmd"))
 
