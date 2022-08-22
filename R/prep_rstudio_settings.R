7555e7e5a52d7a0208bbe83e44024df46636222b
David Kane <dave.kane@gmail.com>
2022-08-22 07:58:45 -0400

    change install_binary to prep_r_studio_settings. Stop messing with .Renviron.

---
+++ R/prep_rstudio_settings.R
@@ -0,0 +1,83 @@
+#' @title Set Up RStudio for Student Use
+#'
+#' @description
+#'
+#' This functions selects RStudio settings which make learning easier. It also
+#' sets the "pkgType" global option to "binary" in your .Rprofile. Students,
+#' especially those on Windows, should never install from source. Doing so fails
+#' too often, and too confusingly.
+#'
+#' You can examine your .Rprofile to confirm this change with
+#' usethis::edit_r_profile()
+#'
+#' @export
+
+prep_rstudio_settings <- function(){
+  
+  # Change default settings in RStudio. Here all are settings:
+  # https://docs.rstudio.com/ide/server-pro/session_user_settings/session_user_settings.html
+  
+  rstudioapi::writeRStudioPreference("save_workspace", "never")
+  rstudioapi::writeRStudioPreference("load_workspace", FALSE)
+  rstudioapi::writeRStudioPreference("insert_native_pipe_operator", TRUE)
+  rstudioapi::writeRStudioPreference("show_hidden_files", TRUE)
+  rstudioapi::writeRStudioPreference("rmd_viewer_type", "pane")
+
+  # Other settings which might be looked at include: packages_pane_enabled,
+  # always_write_history, rainbow_parantheses, show_invisibles,
+  # show_rmd_render_commit, sync_files_pane_working_dir, syntax_color_console
+  # and use_tiny_tex.
+  
+  # This portion of the function is modeled after the function census_api_key()
+  # from the tidycensus package.
+
+  # Get path to user Rprofile and Renviron
+  
+  home <- Sys.getenv("HOME")
+  rprof <- file.path(home, ".Rprofile")
+
+  # Create lines to insert. We added the trailing newline because there is a bug
+  # (?) where the .Rprofile script does not run when the .Rprofile file does not
+  # end with a trailing newline.
+
+  rprof_line <- "options(pkgType = 'binary')\n"
+
+  # If user already has an .Rprofile, then just append to that file
+  # If not, create an .Rprofile in home directory and write to that.
+  
+  if (file.exists(rprof)){
+
+    curr_prof <- readr::read_file(rprof)
+
+
+    # If option already in user's .Rprofile, then just don't write in it
+
+    if (stringr::str_detect(gsub(" ", "", curr_prof), stringr::fixed(gsub(" ", "", rprof_line)))){
+
+      message("Option already in your .Rprofile")
+
+    }else{
+
+      message("Appending new option to your .Rprofile")
+
+      write(paste0(trimws(curr_prof), "\n", rprof_line), file = rprof, append = FALSE)
+
+    }
+  }
+  if (!file.exists(rprof)){
+
+    message("Creating .Rprofile in your home directory")
+
+    file.create(rprof)
+
+    write(rprof_line, file = rprof, append = TRUE)
+
+  }
+
+  # Set pkgType to "binary" for this R session
+
+  options(pkgType = "binary")
+
+  message("You will now only install the binary version of packages")
+
+}
