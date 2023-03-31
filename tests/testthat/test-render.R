20099574af6afe54aad9d21ebf86044779eb7da7
David Kane <dave.kane@gmail.com>
2023-03-31 15:49:47 -0400

    adding renv set up. still does not pass R CMD check when run locally

--- tests/testthat/test-render.R
+++ tests/testthat/test-render.R
@@ -1,13 +1,12 @@
-# For now, we will do all our tutorial testing in this one script. First, we
-# make sure that all the tutorials can be knitted.
-
-tutorial.helpers::knit_tutorials(package = "all.primer.tutorials")
-
-# Ensure that all the tutorials have the default components: copy-code button,
-# information/download pages.
+# For now, we will do all our tutorial testing in this one script.
 
 tutorial_paths <- tutorial.helpers::return_tutorial_paths(package = "all.primer.tutorials")
 
-for(i in tutorial_paths){
-  tutorial.helpers::check_tutorial_defaults(i)
-}
+# First, we make sure that all the tutorials can be knitted.
+
+tutorial.helpers::knit_tutorials(tutorial_paths)
+
+# Second, ensure that all the tutorials have the default components: copy-code button,
+# information/download pages.
+
+tutorial.helpers::check_tutorial_defaults(tutorial_paths)
