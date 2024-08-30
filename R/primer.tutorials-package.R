0c18a86710db3fec8d9f05bd4efc3a08a561adce
Madhavan113 <its1gonbeoff@gmail.com>
2024-08-29 22:04:52 -0400

    Commiting2

--- R/primer.tutorials-package.R
+++ R/primer.tutorials-package.R
@@ -17,7 +17,7 @@ function_for_import  <- function()
 # Best way to demnstrate that quarto is actually "used", thereby stopping
 # renv::status() from bothering us.
 
-library(quart
+library(quarto)
 
 ## usethis namespace: start
 ## usethis namespace: end
